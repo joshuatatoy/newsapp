1333f164f7c5e6d29893a9be3f99ddb3dde693e6
Joshua Tatoy <owentatoy@gmail.com>
2022-04-05 13:03:03 +0800

    Updated logic for search parameters

--- newsappdemo/src/app/newsheadlines/newsheadlines.component.ts
+++ newsappdemo/src/app/newsheadlines/newsheadlines.component.ts
@@ -9,21 +9,33 @@ import { NewsapiservicesService } from '../service/newsapiservices.service';
 })
 export class NewsheadlinesComponent implements OnInit {
 
+  //Variables for Url string concat (Default country is Philippines)
+  countryId = "ph";
+  categoryId = "";
+
   //Reactive form variables and arrays for search bar
   searchForm:FormGroup;
-  countryId: any;
-  categoryId: any;
 
   countries =[
     { id: 1, name: "Philippines"},
     { id: 2, name: "United States"},
     { id: 3, name: "United Kingdom"},
+    { id: 4, name: "Canada"},
+    { id: 5, name: "France"},
+    { id: 6, name: "Australia"},
+    { id: 7, name: "Argentina"},
+    { id: 8, name: "Indonesia"}
   ]
 
   categories =[
     { id: 1, name: "All Category"},
     { id: 2, name: "Business"},
     { id: 3, name: "Entertainment"},
+    { id: 4, name: "General"},
+    { id: 5, name: "Health"},
+    { id: 6, name: "Science"},
+    { id: 7, name: "Sports"},
+    { id: 8, name: "Technology"}
   ]
 
   //Display News Array
@@ -40,18 +52,38 @@ export class NewsheadlinesComponent implements OnInit {
       category: [null]
     });
 
-    this._services.newsHeadlines().subscribe((result)=>{
+    this._services.newsHeadlines(this.countryId, this.categoryId).subscribe((result)=>{
       console.log(result);
       this.newsheadlinesDisplay = result.articles;
     })
   }
 
-  submit() {
+  OnSubmit() {
+    //To validate Id values
     console.log("Form Submitted")
-    console.log(this.searchForm.value)
-    
-    // Get the Id values from country and category options
-    this._services.countryValue= this.searchForm.get('country').value
-    this._services.categoryId= this.searchForm.get('category').value
+    console.log(this.searchForm.value) 
+
+    if(this.searchForm.get('country').value == 1) this.countryId = "ph";
+    else if(this.searchForm.get('country').value == 2) this.countryId = "us";
+    else if(this.searchForm.get('country').value == 3) this.countryId = "gb";
+    else if(this.searchForm.get('country').value == 4) this.countryId = "ca";
+    else if(this.searchForm.get('country').value == 5) this.countryId = "fr";
+    else if(this.searchForm.get('country').value == 6) this.countryId = "au";
+    else if(this.searchForm.get('country').value == 7) this.countryId = "ar";
+    else if(this.searchForm.get('country').value == 8) this.countryId = "id";
+
+    if(this.searchForm.get('category').value == 1) this.categoryId = "";
+    else if(this.searchForm.get('category').value == 2) this.categoryId = "business";
+    else if(this.searchForm.get('category').value == 3) this.categoryId = "entertainment";
+    else if(this.searchForm.get('category').value == 4) this.categoryId = "general";
+    else if(this.searchForm.get('category').value == 5) this.categoryId = "health";
+    else if(this.searchForm.get('category').value == 6) this.categoryId = "science";
+    else if(this.searchForm.get('category').value == 7) this.categoryId = "sports";
+    else if(this.searchForm.get('category').value == 8) this.categoryId = "technology";
+
+    this._services.newsHeadlines(this.countryId, this.categoryId).subscribe((result)=>{
+      console.log(result);
+      this.newsheadlinesDisplay = result.articles;
+    })
   }
 }
--- newsappdemo/src/app/service/newsapiservices.service.ts
+++ newsappdemo/src/app/service/newsapiservices.service.ts
@@ -8,23 +8,16 @@ import { Observable } from 'rxjs';
 export class NewsapiservicesService {
 
   constructor(private _http:HttpClient) { }
-
-  // Variables for News API url
-  countryId = "ph";
-  categoryId = "";
-  apiKey = "c9704f4ddce7497d9cdf0b4119ecc105";
-  countryValue: any;
   
-  // News API url
-  newsApiUrl = "https://newsapi.org/v2/top-headlines" + 
-               "?country=" + this.countryId + 
-               "&category=" + this.categoryId + 
-               "&apiKey=" + this.apiKey;
+  //News API url and API key
+  newsApiUrl = "https://newsapi.org/v2/top-headlines";
+  apiKey = "c9704f4ddce7497d9cdf0b4119ecc105";
 
-  // newsheadlines()
-  newsHeadlines():Observable<any>
+  newsHeadlines(country: string, category: string):Observable<any>
   {
-    return this._http.get(this.newsApiUrl);
+    return this._http.get(this.newsApiUrl + 
+                          '?country='+ country + 
+                          '&category=' + category + 
+                          '&apikey=' + this.apiKey);
   }
-
 }
